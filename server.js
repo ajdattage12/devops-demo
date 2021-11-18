70025436bab1c93db775cad3e03340d18a514f57
Ariell Dattage <ariell.dattage@Ariells-MBP.localdomain>
2021-11-18 12:07:08 -0700

    initalize rollbar

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 const express = require('express');
 const path = require('path');
 const cors = require('cors');
+//include and initalize the rollbar library with you access token
+var Rollbar = require('rollbar')
+var rollbar = new Rollbar({
+  accessToken: '1508765836a44e269922ed11acee80fa',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
+// record a generic message and send it to Rollbar
+rollbar.log('Hello world!')
 
 const app = express();
 
@@ -8,11 +17,34 @@ const students = ["Ariell"];
 
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "/public/index.html"));
+    rollbar.info("HTML file served successfully");
 });
 
 app.get("/api/students", (req, res) => {
+    rollbar.info("Someone got the list of students on page load");
     res.status(200).send(students);
 });
+
+app.post("/api/students", (req, res) => {
+    let {name} = req.body;
+    name = name.trim();
+
+    const index = students.findIndex((studentName) => studentName === name);
+
+    if(index === -1 && name !== ""){
+        students.push(name);
+        rollbar.log("Student added successfully", {author: "Ariell", type: "manual entry"});
+        res.status(200).send(students);
+    } else if (name === "") {
+        rollbar.error("No name given");
+        res.status(400).send("Must provide a name");
+    } else {
+        rollbar.error("Student already exists");
+        res.status(400).send("That student already exists");
+    }
+})
+
+app.use(rollbar.errorHandler());
 
 const port= process.env.PORT || 4545;
 

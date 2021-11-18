9f0f8b4edd333f4981d149def66a803ca1ddf4fb
Ariell Dattage <ariell.dattage@Ariells-MBP.localdomain>
2021-11-18 11:29:52 -0700

    inital

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const path = require('path');
+const cors = require('cors');
+
+const app = express();
+
+const students = ["Ariell"];
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "/public.index.html"));
+});
+
+app.get("/api/students", (req, res) => {
+    res.status(200).send(students);
+});
+
+const port= process.env.PORT || 4545;
+
+app.listen(port, () => {
+    console.log(`Jammin on ${port}`);
+});
